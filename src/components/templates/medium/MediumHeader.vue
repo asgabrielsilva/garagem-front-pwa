91eb8681bfddc54d197c77a3af54f673a33acb6d
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-07-22 15:16:40 -0300

    Preparando o ambiente baseado no front da garagem e o fake store

---
+++ src/components/templates/medium/MediumHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="icons">
+    <i class="mdi mdi-magnify" />
+  </div>
+</template>
+
+<style scoped>
+.icons {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+</style>
