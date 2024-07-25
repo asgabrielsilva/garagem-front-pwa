f6f47c1589439e8d362a9bf8a037ae5dbd039fba
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-07-25 11:10:03 -0300

    LayoutSmall pronto porém tenho que mostrar para o professor

--- src/components/templates/small/SmallFooter.vue
+++ src/components/templates/small/SmallFooter.vue
@@ -1,39 +1,59 @@
 <script setup>
 import { ref } from 'vue';
 import { onBeforeRouteUpdate } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
 
 const showMenu = ref(false);
+const confirmLogout = ref(false);
 
 onBeforeRouteUpdate(() => {
   showMenu.value = false;
 });
+onBeforeRouteUpdate(() => {
+  confirmLogout.value = false;
+});
 
 </script>
 <template>
+<div id="logoutConfirm" :style="{ display: confirmLogout ? 'block' : 'none' }">
+    <div class="btn" @click="confirmLogout = !confirmLogout">
+      Tem certeza?
+    </div>
+    <router-link to="/logout">
+        Sim
+    </router-link>
+    
+  </div>
+
   <div id="footerMenu" :style="{ display: showMenu ? 'block' : 'none' }">
-    <RouterLink to="/">
-      <i class="mdi mdi-home-outline" />
-      Home
-    </RouterLink>
-    <RouterLink to="/login">
-      <i class="mdi mdi-account" />
-      Login
-    </RouterLink>
+    <router-link to="/acessorios">
+        Acessorios
+      </router-link>
+      <router-link to="/categorias">
+         Categorias
+      </router-link>
+      <router-link to="/cores">
+         Cores
+      </router-link>
+      <router-link to="/marcas">
+         Marcas
+      </router-link>
   </div>
   <div class="icons">
     <RouterLink to="/">
       <i class="mdi mdi-home-outline" />
       Home
     </RouterLink>
-    <RouterLink to="/">
-      <i class="mdi mdi-account-circle-outline" />
-      Perfil
-    </RouterLink>
-    <RouterLink to="/">
-      <i class="mdi mdi-cart-outline" />
-      Carrinho
-    </RouterLink>
-
+    <div v-if="authStore.loggedIn">
+    <div class="hamburger" @click="confirmLogout = !confirmLogout">
+      <i class="icon mdi mdi-account" /> Logout
+    </div>
+    </div>
+    <router-link v-else to="/login">
+    <i class="icon mdi mdi-account" /> Login
+    </router-link>
     <div class="hamburger" @click="showMenu = !showMenu">
       <i class="mdi mdi-menu" />
       Menu
@@ -46,7 +66,7 @@ onBeforeRouteUpdate(() => {
     bottom: 15%;
     right: 0;
 
-    width: 20%;
+    width: 30%;
     border-top: #EEEEEE 1px solid;
     background-color: white;
 
@@ -62,6 +82,32 @@ onBeforeRouteUpdate(() => {
     color: #282828;
     font-size: 1rem;
     transition: color 0.3s;
+}
+#logoutConfirm {
+    position: fixed;
+    bottom: 13%;
+    right: 30%;
+
+    width: 40%;
+    border-top: #EEEEEE 1px solid;
+    background-color: white;
+
+    display: block;
+    padding: 1rem;
+
+   
+}
+
+#logoutConfirm a {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: space-between;
+    text-decoration: none;
+    color: #282828;
+    font-size: 1rem;
+    transition: color 0.3s;
+    padding-top: 8px;
 }
 
 
@@ -85,5 +131,10 @@ onBeforeRouteUpdate(() => {
 
 .hamburger:hover {
     cursor: pointer;
+}
+
+.btn{
+  font-size: 1.1rem;
+  color: #282828;
 }
 </style>
